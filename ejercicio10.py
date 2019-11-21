7c44c3a7eb6147d14ae8ff9113aac5485fdeb8a5
andreserb <58049565+andreserb@users.noreply.github.com>
2019-11-21 16:52:20 -0500

    Update ejercicio10.py
    Escribir un programa que pida al usuario un número entero y 
    muestre por pantalla un triángulo rectángulo como el de más abajo.

--- ejercicio10.py
+++ ejercicio10.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 #Escribir un programa que pida al usuario un número entero y 
 #muestre por pantalla un triángulo rectángulo como el de más abajo.
+
 lista=[]
 a=int(input("Ingrese la altura del triángulo (entero positivo): "))
+
 for i in range(1,a+1,2):
     lista.append(i)
     print(' '.join(map(str, lista[::-1])))
